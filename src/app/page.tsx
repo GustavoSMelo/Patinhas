06782d356fec98a7d75e947d60bfc56120423af7
GustavoSMelo <gsantos15569@gmail.com>
2024-01-30 19:58:56 -0300

    finished slide images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,54 @@
-import pet01 from '../../public/images/pet01.jpg';
-import pet02 from '../../public/images/pet02.jpg';
-import pet03 from '../../public/images/pet03.jpeg';
-import pet04 from '../../public/images/pet04.jpg';
-import pet05 from '../../public/images/pet05.jpg';
-import Portrait from './components/portrait';
+import imagesPetsArray from "@catsProject/helper/homeSlideHelper";
+import Portrait from "@catsProject/components/portrait";
 
 const Home = () => {
+    const renderSlideShow = () => {
+        const slideLength = 12;
+        const slides = [];
+
+        for (let i = 0; i < slideLength; i++) {
+            slides.push(imagesPetsArray[i].src);
+        }
+
+        return slides.map((imageSource) => (
+            <Portrait image={imageSource} key={imageSource} />
+        ));
+    };
+
     return (
         <main className="bg-slate-900 max-h-full text-white">
             <div className="pt-10 flex align-middle justify-center pb-12">
-                <h1 className="text-6xl font-extrabold tracking-wider  font-breeSerif">Patinhas</h1>
+                <h1 className="text-6xl font-extrabold tracking-wider  font-breeSerif">
+                    Patinhas
+                </h1>
             </div>
 
-            <p className="text-center text-xl">Crie e compartilhe momentos unicos com seu pet</p>
+            <p className="text-center text-xl">
+                Crie e compartilhe momentos unicos com seu pet
+            </p>
 
-            <div className='pb-6 px-7 mt-32 flex flex-row animate-home-slide'>
-                <Portrait image={pet01.src} />
-                <Portrait image={pet02.src} />
-                <Portrait image={pet03.src} />
-                <Portrait image={pet04.src} />
-                <Portrait image={pet05.src} />
-                <Portrait image={pet01.src} />
-                <Portrait image={pet01.src} />
-                <Portrait image={pet01.src} />
-                <Portrait image={pet01.src} />
-                <Portrait image={pet01.src} />
-                <Portrait image={pet01.src} />
-                <Portrait image={pet01.src} />
+            <div className="pb-6 px-7 mt-32 flex flex-row animate-home-slide">
+                {renderSlideShow()}
             </div>
+
+            <p className="text-center text-xl mt-32">
+                Criamos uma foto polaroid digital do seu bixinho e voce pode
+                compartilhar e baixar a foto para imprimir
+                <br />
+                Porem caso voce queira eternizar seu melhor amigo de forma
+                digital, aqui voce pode criar um album de fotos digital
+            </p>
+
+            <p className="text-center text-xl mt-10">
+                Para criarmos sua conta, nao pedimos informacoes sigilosas nem
+                senha nem coisas parecidas, entao pode ficar tranquilo :D
+            </p>
+
+            <p className="text-center mt-24 font-medium cursor-pointer mb-6">
+                Ficou interessado ? <button type="button" className="bg-yellow-300 text-black px-3 py-3 rounded-md">Crie sua conta</button>
+            </p>
+
+
         </main>
     );
 };
---
+++ src/helper/homeSlideHelper.ts
@@ -0,0 +1,29 @@
+import pet01 from '../../public/images/pet01.jpg';
+import pet02 from '../../public/images/pet02.jpg';
+import pet03 from '../../public/images/pet03.jpeg';
+import pet04 from '../../public/images/pet04.jpg';
+import pet05 from '../../public/images/pet05.jpg';
+import pet06 from '../../public/images/pet06.jpg';
+import pet07 from '../../public/images/pet07.jpg';
+import pet08 from '../../public/images/pet08.jpg';
+import pet09 from '../../public/images/pet09.jpg';
+import pet10 from '../../public/images/pet10.jpg';
+import pet11 from '../../public/images/pet11.jpg';
+import pet12 from '../../public/images/pet12.jpg';
+
+const objectPetImages = [
+    pet01,
+    pet02,
+    pet03,
+    pet04,
+    pet05,
+    pet06,
+    pet07,
+    pet08,
+    pet09,
+    pet10,
+    pet11,
+    pet12,
+]
+
+export default objectPetImages;
